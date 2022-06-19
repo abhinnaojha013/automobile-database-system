0013ec1c10706cb7c480e05acd25a230a3b61ef6
Abhinna <abhinnacsit18@oic.edu.np>
2022-06-19 12:26:17 +0545

    Added few pages and beautified

---
+++ Controllers/AutoController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace automobile_database_system.Controllers
+{
+    public class AutoController : Controller
+    {
+        // GET: AutoController
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "User";
+            ViewBag.Action = "Index";
+            return View();
+        }
+
+        // GET: AutoController/Details/5
+        public ActionResult Details(int id)
+        {
+            ViewBag.Controller = "User";
+            ViewBag.Action = "Index";
+            return View();
+        }
+
+        // GET: AutoController/Create
+        public ActionResult Create()
+        {
+            ViewBag.Controller = "User";
+            ViewBag.Action = "Index";
+            return View();
+        }
+
+        // POST: AutoController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AutoController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Controller = "User";
+            ViewBag.Action = "Index";
+            return View();
+        }
+
+        // POST: AutoController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AutoController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AutoController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
